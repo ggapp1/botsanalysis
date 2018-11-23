69c227834159694a195bb55c809a7e6c78d3186d
ggapp <gustavo.germano1@gmail.com>
2018-11-23 16:44:33 -0200

    collected few hshtags

--- data.py
+++ data.py
@@ -24,12 +24,18 @@ def generate_Botometer():
 	access_token = '513768992-JfWa1xMpBFKQNWfHlUncCVDCPxDOUVs8shUegVQU'
 	access_token_secret= 'tRQcBExN1vG6otAZnM0L2QHyUupM2qJIz04wcFqkCYoVY'
 
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_token, access_token_secret)
+	mashape_key = "RWoa8U8jiImshcKeqzezdmcSr6M7p1U1plajsnTa1CF4tlKGTh"
+	twitter_app_auth = {
+    'consumer_key': consumer_key,
+    'consumer_secret': consumer_secret,
+    'access_token': access_token,
+    'access_token_secret': access_token_secret,
+  	}
 
-	api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
-	
-	return api
+	bom = botometer.Botometer(wait_on_ratelimit=True,
+                          mashape_key=mashape_key,
+                          **twitter_app_auth)
+	return bom
 
 def limit_handler(cursor):
 	#deal with timeouts
@@ -160,39 +166,86 @@ def get_bots_by_hashtag(hashtag, api, bom):
 	bots_file.close() 
 
 def get_tweets(user_id, api):
-	print("test")
+	print("- Getting tweets...")
 	tweets = [[]]
 	tweet_list = []
+	pages = tweepy.Cursor(api.user_timeline, id=user_id, count=200,timeout=600,lsinclude_rts = False,tweet_mode='extended'
+	).pages()  
+	sleeptime = 1
+	while True:
+	    try:
+	        page = next(pages)
+	        for tweet in page:
+	        	tweet_list.append(tweet)
+	        time.sleep(sleeptime)
+
+	    except tweepy.TweepError as e: #taking extra care of the "rate limit exceeded"
+	        print(e.api_code)
+	        time.sleep(60) 
+	        try:
+	        	page = next(pages)
+	        	for tweet in page:
+	        		tweet_list.append(tweet)
+	        		time.sleep(sleeptime)
+
+	        except tweepy.TweepError as e:
+	            print(e.api_code)
+	            print("failed, user probably has protected account")
+	            print(user_id)
+	            return ids
+
+	    except StopIteration:
+	        break
+	"""
 
 	for pages in tweepy.Cursor(api.user_timeline, id=user_id, count=200,timeout=600,lsinclude_rts = False,tweet_mode='extended'
 		).pages():        
 		for tweet in pages:
 			tweet_list.append(tweet)
-	
+	"""
 	print("len "+str(len(tweet_list)))
 
-	for tweet in tweet_list: 	
+	for tweet in tweet_list:	
 	  if 'retweeted_status' in dir(tweet):
-	    text = tweet.retweeted_status
+	    text = tweet.retweeted_status.full_text
 	    created_at = tweet.retweeted_status.created_at
-	    replies = tweet.retweeted_status.reply_count
 	    favs = tweet.retweeted_status.favorite_count
 	    retweets = tweet.retweeted_status.retweet_count
+
+	    if tweet.retweeted_status.entities['hashtags']:
+	    	hashtags = tweet.retweeted_status.entities['hashtags'][0]['text']
+	    else:
+	    	hashtags = ""
+
+	    if tweet.retweeted_status.entities['urls']:
+	    	url = tweet.retweeted_status.entities['urls'][0]['expanded_url']
+	    else:
+	    	url = ""
+
 	    is_retweet = 1
 	  else:
-	    text = tweet.created_at
+	    text = tweet.full_text
 	    created_at = tweet.created_at 
-	    replies = tweet.reply_count
 	    favs = tweet.favorite_count
 	    retweets = tweet.retweet_count
+
+	    if tweet.entities['hashtags']:
+		    hashtags = tweet.entities['hashtags'][0]['text']
+	    else:
+	        hashtags = ""
+
+	    if tweet.entities['urls']:
+	    	url = tweet.entities['urls'][0]['expanded_url']
+	    else:
+	    	url = ""
+
 	    is_retweet = 0
 
 	  nfkd_form = unicodedata.normalize('NFKD', str(text))
 	  clean_text = nfkd_form.encode('ASCII', 'ignore') 
-
-	  tweet_data = [clean_text, created_at, replies, favs, retweets, is_retweet]
+	  tweet_data = [str(clean_text), str(hashtags),str(url), str(created_at), str(favs), str(retweets), str(is_retweet)]
 	  tweets.append(tweet_data)
-
+	print("+ User has {} tweets.".format(len(tweets)))  
 	return tweets
 
 
--- tweet_wrapper.py
+++ tweet_wrapper.py
@@ -10,30 +10,29 @@ import unicodedata
 
 api = data.generate_API()
 
-mashape_key = "RWoa8U8jiImshcKeqzezdmcSr6M7p1U1plajsnTa1CF4tlKGTh"
-
-bom = botometer.Botometer(wait_on_ratelimit=True,
-                          mashape_key=mashape_key,
-                          **api)
+bom = data.generate_Botometer()
 
 def main():
-  users = [114312114]
-  for user in users:
-    user_file = open("files/tweets_"+str(user), "w+")
+  init = open('visited').read().splitlines()
+  users = [int(i) for i in init]
+  print(len(users))
+  wrapped = 0
+  for user_id in users:
+    user_file = open("files/tweets/tweets_"+str(user_id), "w+")
     user_score = data.bot_score(user_id, bom)
-    user_obj = api.get_user(user)
-
-    user_file.write("{},{}\n".format(user_id, user_score))
-    user_file.write("{},{},{}\n".format(user_obj.follower_count, user_obj.friends_count, user_obj.statuses_count))
+    user_obj = api.get_user(user_id)
+    user_file.write("{},{}".format(user_id, user_score))
+    user_file.write("{},{},{}\n".format(user_obj.followers_count, user_obj.friends_count, user_obj.statuses_count))
     user_file.write("{}\n".format(user_obj.created_at))
 
-    tweets = data.get_tweets(user, api)
+    tweets = data.get_tweets(user_id, api)
 
     for tweet in tweets:
-      user_file.write(','.join(tweet))
+      user_file.write(','.join(tweet)+"\n")
 
     user_file.close()
-
+    wrapped = wrapped + 1
+    print("# got user. Count: "+str(wrapped))
 
 if __name__ == '__main__':
   main()
